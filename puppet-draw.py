d32f2d9f3dfcdc10f30e14098d73d4870ea1ae5e
hyeon98 <olreng97@gmail.com>
2022-04-03 00:37:43 +0900

    20220403_1

---
+++ puppet-draw.py
@@ -0,0 +1,48 @@
+# https://programmers.co.kr/learn/courses/30/lessons/64061
+
+
+inputBoard = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
+inputMoves = [1, 5, 3, 5, 1, 2, 1, 4]
+
+
+def solution(board, moves):
+
+    countBoard = 0
+    countScore = 0
+    pickUpItem = []
+    for indexMoves in moves:
+        # print(indexMoves)
+        for index1Board in board:
+            # print(index1Board)
+            if index1Board[indexMoves-1] != 0:
+                pickUpItem.append(index1Board[indexMoves-1])
+                index1Board[indexMoves-1] = 0
+                
+                # [4, 3, 1, 1, 3, 2, 4]
+                countItem = 0
+                prevPickUpItem = 0
+                for indexPickUpItem in pickUpItem:
+                    if prevPickUpItem == indexPickUpItem:
+                        # print('pickUpItem', pickUpItem)
+                        # print('countItem', countItem)
+                        countScore += 1
+                        # print('pickUpItem', pickUpItem[countItem])
+                        del pickUpItem[countItem]
+                        # print('pickUpItem-1', pickUpItem[countItem-1])
+                        del pickUpItem[countItem-1]
+                    else:
+                        prevPickUpItem = indexPickUpItem
+                    
+                    countItem += 1
+                break
+            # print(index1Board[indexMoves+1])
+
+    # print(inputBoard)
+    # print(pickUpItem)
+    # print(countScore)
+
+    answer = countScore*2
+    return answer
+
+print(solution(inputBoard, inputMoves))
+
